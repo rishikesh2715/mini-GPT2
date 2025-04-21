57df0b3787195d0de042e9f442091878241e436b
rishi <rishikesh2715@gmail.com>
2025-04-21 00:05:06 -0500

    added TODO's tasks

---
+++ data/preprocess.py
@@ -0,0 +1,10 @@
+"""
+TODO:
+
+Clean the text (remove unwanted characters, normalize)
+- Use tokenizer to convert text to token IDs
+- Split dataset into train and val sets
+- Save token ID datasets as binary or torch tensors (e.g., train.bin)
+
+
+"""
---
+++ scripts/evaluate.py
@@ -0,0 +1,9 @@
+"""
+# ✅ TODO:
+│   - Load model and val data
+│   - Compute loss and perplexity
+│   - Optionally: BLEU or other text metrics
+│   - Print and log results
+│
+
+"""
